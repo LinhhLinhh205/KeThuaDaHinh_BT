8acb3634eb5538e968226b2a55121c1790f52bcb
Ngọc Linh <linhlinhbehy@gmail.com>
2024-01-12 10:04:35 +0700

    Add project files.

---
+++ BT01/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT01
+{
+    class Account
+    {
+        private double _balance;
+        protected double Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+        public Account()
+        {
+
+        }
+
+        public Account(double balance)
+        {
+            this._balance = balance;
+        }
+        public virtual bool Withdraw(double amount)
+        {
+            return false;
+        }
+        public virtual bool Deposit(double amount)
+        {
+            return false;
+        }
+        public virtual void PrintBalance()
+        {
+            Console.WriteLine($"So du:{_balance}");
+        }
+    }
+    class SavingAccount : Account
+    {
+        private double _interesRate = 0.8;
+        public double InteresRate
+        {
+            get { return _interesRate; }
+            set { _interesRate = value; }
+        }
+        public SavingAccount(double balance) : base(balance)
+        {
+
+        }
+        public override bool Withdraw(double amount)
+        {
+            if (0 < amount && amount <= Balance)
+            {
+                Balance = Balance - (amount + amount * InteresRate);
+
+                Console.WriteLine($"Rut tien thanh cong.So tien da rut la:{amount}");
+                return true;
+            }
+
+
+            else Console.WriteLine("Giao dich that bai");
+            return false;
+        }
+        public override bool Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Balance = Balance + (amount + amount * InteresRate);
+
+                Console.WriteLine($"Them tien thanh cong.So tien da them vao so du:{amount}");
+                return true;
+            }
+            else Console.WriteLine("Them tien that bai");
+            return false;
+        }
+        public override void PrintBalance()
+        {
+            Console.WriteLine($"The saving account balance is:{Balance}");
+        }
+
+    }
+    class CheckingAccount : Account
+    {
+        public CheckingAccount(double balance) : base(balance)
+        {
+
+        }
+        public override bool Withdraw(double amount)
+        {
+            if (amount > 0 && amount < Balance)
+            {
+                Balance = Balance - amount;
+
+                Console.WriteLine($"Giao dich thanh cong.So tien da rut la:{amount}");
+                return true;
+
+            }
+
+
+            else Console.WriteLine("Giao dich that bai");
+            return false;
+
+        }
+        public override bool Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Balance = Balance + amount;
+
+                Console.WriteLine($"Them tien thanh cong.So tien da them:{amount}");
+                return true;
+            }
+            else Console.WriteLine("Them tien that bai");
+            return false;
+        }
+        public override void PrintBalance()
+        {
+            Console.WriteLine($"The checking account balance is:{Balance}");
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+
+                Console.WriteLine("SavingsAccount");
+                Account ac2 = new SavingAccount(2000);
+                ac2.Withdraw(200);
+                ac2.Deposit(400);
+                ac2.PrintBalance();
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("CheckingAccount");
+                Account ac1 = new CheckingAccount(1000);
+                ac1.Withdraw(200);
+                ac1.Deposit(500);
+                ac1.PrintBalance();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ BT02/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT02
+{
+    class SanPham
+    {
+        private string _ten;
+        private double _giamua;
+
+        public string Ten
+        {
+            get { return _ten; }
+
+        }
+        public double Giamua
+        {
+            get { return _giamua; }
+            set
+            {
+                if (value >= 0)
+                    _giamua = value;
+            }
+        }
+        public SanPham()
+        {
+        }
+        public SanPham(string ten, double giamua)
+        {
+            this._ten = ten;
+            this._giamua = giamua;
+        }
+        public virtual double TinhGiaBan()
+        {
+            return 0;
+        }
+        public virtual void InChiTiet()
+        {
+            Console.WriteLine($"Ten san pham:{_ten}");
+        }
+        public virtual void Nhap()
+        {
+            Console.Write("Nhap ten san pham:");
+            _ten = Console.ReadLine();
+            Console.Write("Nhap gia mua cua san pham:");
+            _giamua = double.Parse(Console.ReadLine());
+        }
+    }
+    class Socola : SanPham
+    {
+        private double _loinhuan;
+        public Socola()
+        {
+        }
+        public Socola(string ten, double giamua) : base(ten, giamua)
+        {
+        }
+        public override double TinhGiaBan()
+        {
+            _loinhuan = Giamua * 0.2;
+            return Giamua + _loinhuan;
+        }
+        public override void InChiTiet()
+        {
+            base.InChiTiet();
+            Console.WriteLine($"Gia ban:{TinhGiaBan()}");
+        }
+        public override void Nhap()
+        {
+            base.Nhap();
+        }
+    }
+    class NuocUong : SanPham
+    {
+        private double _loinhuan;
+        private double _chiphigiulanh;
+
+        public NuocUong()
+        {
+        }
+        public NuocUong(string ten, double giamua) : base(ten, giamua)
+        {
+        }
+        public override double TinhGiaBan()
+        {
+            _loinhuan = Giamua * 0.15;
+            _chiphigiulanh = Giamua * 0.1;
+            return Giamua + _loinhuan + _chiphigiulanh;
+        }
+        public override void InChiTiet()
+        {
+            base.InChiTiet();
+            Console.WriteLine($"Gia ban:{TinhGiaBan()}");
+        }
+        public override void Nhap()
+        {
+            base.Nhap();
+        }
+    }
+    class QuanLySanPham 
+    {
+        private string _ten = "Cua Hang Ban Le";
+        private SanPham[] _danhSachSP;
+        public void Nhap()
+        {
+            Console.Write("Nhap so luong san pham:");
+            int soluong = int.Parse(Console.ReadLine());
+
+            _danhSachSP = new SanPham[soluong];
+
+            for (int i = 0; i < soluong; i++)
+            {
+
+                Console.WriteLine("------Quan Ly San Pham------");
+                Console.WriteLine("1. Socola");
+                Console.WriteLine("2. Nuoc uong");
+                Console.Write("Chon loai san pham:");
+                int chon = int.Parse(Console.ReadLine());
+
+                if (chon == 1)
+                    _danhSachSP[i] = new Socola();
+                else if (chon == 2)
+                    _danhSachSP[i] = new NuocUong();
+                _danhSachSP[i].Nhap();
+            }
+        }
+        public void InDSSanPham()
+        {
+            Console.WriteLine($"Danh sach san pham cua {_ten}");
+            for (int i = 0; i < _danhSachSP.Length; i++)
+            {
+                _danhSachSP[i].InChiTiet();
+
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                QuanLySanPham quanlySP = new QuanLySanPham();
+                quanlySP.Nhap();
+                quanlySP.InDSSanPham();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
